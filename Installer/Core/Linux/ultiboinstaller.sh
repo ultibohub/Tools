1418729f3764c35546e26d84be420479306e3e76
Ultibo <admin@ultibo.org>
2023-03-06 11:38:53 +1100

    Update installer script for Lazarus 2.2.6 stable (2.2.7 fixes)

--- Installer/Core/Linux/ultiboinstaller.sh
+++ Installer/Core/Linux/ultiboinstaller.sh
@@ -47,9 +47,9 @@ esac
 FPC_BUILD=3.2.2
 FPC_BRANCH="ultibo-3.2.2"
 FPC_NONSTABLE=""
-LAZARUS_BUILD="2.2.4U"
+LAZARUS_BUILD="2.2.6U"
 LAZARUS_CONFIG="110"
-LAZARUS_BRANCH="ultibo-2.2.4" 
+LAZARUS_BRANCH="ultibo-2.2.6" 
 
 # The Git branch of Ultibo Core we are building
 ULTIBO_BRANCH="master"
@@ -72,7 +72,7 @@ if [ $# -ge 1 ]; then
 			FPC_BUILD=3.2.3
 			FPC_BRANCH="ultibo-3.2"
 			FPC_NONSTABLE="FIXES"
-			LAZARUS_BUILD="2.2.5U"
+			LAZARUS_BUILD="2.2.7U"
 			LAZARUS_CONFIG="110"
 			LAZARUS_BRANCH="ultibo-2.2"
 			;;
